23b6f76c8d499884293b73a2ecd1d6820072b63e
Tallerr <malashta@gmail.com>
2020-01-12 18:39:19 +0700

    create first structure

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './ActiveQuiz.module.css'
+
+const ActiveQuiz = props => (
+  <div className={classes.ActiveQuiz}>
+    <p className={classes.Question}>
+      <span>
+        <strong>
+          2.
+        </strong>&nbsp;
+        test
+      </span>
+
+      <small>4 of 12</small>
+    </p>
+
+    <ul>
+      <li>1.</li>
+      <li>2.</li>
+      <li>3.</li>
+      <li>4.</li>
+    </ul>
+  </div>
+);
+
+export default ActiveQuiz
