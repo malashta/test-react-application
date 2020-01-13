7447260dd4ddd904c00d9563b1db7c7491e03a12
Tallerr <malashta@gmail.com>
2020-01-13 17:46:33 +0700

    Add pages components

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classes from './QuizCreator.module.css'
+
+class QuizCreator extends Component {
+  static defaultProps = {
+
+  };
+
+  static propTypes = {};
+
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Quiz Creator</h1>
+      </div>
+    );
+  }
+}
+
+export default QuizCreator;
---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classes from 'QuizList';
+
+class QuizList extends Component {
+  static defaultProps = {
+
+  };
+
+  static propTypes = {};
+
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Quiz List</h1>
+      </div>
+    );
+  }
+}
+
+export default QuizList;
+
