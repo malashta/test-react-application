349d707a43d2f12afc200eef3d3278ae914cc9ba
Tallerr <malashta@gmail.com>
2020-01-13 21:54:16 +0700

    form auth validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import classes from './Auth.module.css';
 import Button from "../../components/ui/Button/Button";
 import Input from "../../components/ui/Input/Input";
+import is from 'is_js';
 
 class Auth extends Component {
 
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -38,8 +40,48 @@ class Auth extends Component {
 
   registerHandler = () => {};
 
+  validateControl(value, validation) {
+    if (!validation) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (validation.email) {
+      isValid = is.email(value) && isValid;
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
   onChangeHandler = (event, controlName) => {
-    console.log(`${controlName}: `, event.target.value);
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+
+    formControls[controlName] = control;
+
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+
+    this.setState({
+      formControls,
+      isFormValid
+    });
   };
 
   submitHandler = event => {
@@ -78,12 +120,14 @@ class Auth extends Component {
             <Button
               type="success"
               onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Login
             </Button>
             <Button
               type="primary"
               onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
             >
               Register
             </Button>
